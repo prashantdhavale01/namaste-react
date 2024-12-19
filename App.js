4623116de5346f6067ae75aac29e7215e0618540
prashantdhavale01 <prashantdhavale01@gmail.com>
2024-12-19 20:59:23 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div", {id: "parent"},
+  [React.createElement("div", {id: "child1"}, [
+    React.createElement("h1", {}, "H1 Tag"),
+    React.createElement("h2", {}, "H2 Tag"),
+  ]),
+  React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {}, "H1 Tag"),
+    React.createElement("h2", {}, "H2 Tag"),
+  ])]
+);
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
